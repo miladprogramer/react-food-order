1106f75ccc08a7c75dc8b3b26f5e112da5da7b2b
meysam <m.monfared83@gmail.com>
2023-09-20 16:40:07 +0330

    edit footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <>
     <div className='footer'>
+        <Container>
         <Row>
             <Col className='lg-3 md-6 footer-logo'>
             <img src={logo}/>
@@ -49,6 +50,36 @@ const Footer = () => {
        </div>
             </Col>
         </Row>
+
+        <Row>
+            <Col className='lg-6'>
+<p className='copyright'>CopyRight-2022 Website made by MEYSAM -- All right reserved</p>
+            </Col>
+            <Col className='lg-6'>
+                <div className='socials d-flex gap-4 justify-content-end'>
+                <span className='m-0'>Follow:</span>
+                <span>
+<i class="ri-facebook-circle-fill"></i>
+                </span>
+                <span>
+<i class="ri-twitter-fill"></i>
+</span>
+<span>
+<i class="ri-github-fill"></i>
+</span>
+<span>
+<i class="ri-linkedin-box-fill"></i>
+</span>
+
+
+
+
+                </div>
+
+            </Col>
+        </Row>
+        </Container>
+       
     </div>
     </>
   )
