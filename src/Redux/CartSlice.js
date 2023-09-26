dc8fd9eb0c2f48f4dd6775a20f30a4af15cfe6ca
meysam <m.monfared83@gmail.com>
2023-09-26 18:07:06 +0330

    edit in cart slice

--- src/Redux/CartSlice.js
+++ src/Redux/CartSlice.js
@@ -35,7 +35,42 @@ state.totalAmount=state.cartItems.reduce(
     (total,item)=>total + Number(item.price) * Number(item.quantity),0
 )
 
-    }
+    },
+
+    deleteItem(state,action){
+        const id=action.payload
+        const existingItem=state.cartItems.find((item)=>item.id===id)
+
+  state.cartItems=state.cartItems.filter((item)=>item.id !==id)
+
+  state.totalQuantity = state.totalQuantity - existingItem.quantity;
+
+state.totalAmount=state.cartItems.reduce(
+    (total,item)=>total + Number(item.price) * Number(item.quantity),0
+)
+      
+    },
+
+    removeItem(state,action){
+        const id=action.payload
+        const existingItem=state.cartItems.find((item)=>item.id===id)
+       
+
+
+        if (existingItem.quantity === 1) {
+            state.cartItems = state.cartItems.filter((item) => item.id !== id);
+          } else {
+            existingItem.quantity--;
+            existingItem.totalPrice =
+              Number(existingItem.totalPrice) - Number(existingItem.price);
+          }
+    
+          state.totalAmount = state.cartItems.reduce(
+            (total, item) => total + Number(item.price) * Number(item.quantity),
+            0
+          );
+    },
+
    
   },
 })
