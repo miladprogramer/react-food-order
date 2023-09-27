417d2461dec8185348a3ab0fa4f79ceba4c7d14f
meysam <m.monfared83@gmail.com>
2023-09-27 22:48:43 +0330

    edit search-box

--- src/components/AllFoods.jsx
+++ src/components/AllFoods.jsx
@@ -1,15 +1,15 @@
 
-import {React,useState} from 'react'
+import { React, useState } from 'react'
 
-import { Container,Col,Row } from 'reactstrap'
+import { Container, Col, Row } from 'reactstrap'
 import './Style.css'
 
 import AllProducts from '../Fake-Data/FakeData'
 import Foods from './Foods'
 const AllFoods = () => {
 
-  const [searchItem,setSearchItem]=useState('')
-  
+  const [searchItem, setSearchItem] = useState('')
+
   const searchedProduct = AllProducts.filter((item) => {
     if (searchItem === "") {
       return item;
@@ -20,39 +20,39 @@ const AllFoods = () => {
   });
 
   return (
-  <>
- 
-   < div className='background-title'>
-  
-<h2>All Foods</h2>
-    </div>
-  
-  <Container>
-    <Row>
-      <Col lg="3">
-      <div className='search-box d-flex justify-content-between mt-5 mb-5'>
-      <input value={searchItem} onChange={e=> setSearchItem((e).target.value)}
-       type='text' placeholder='Search'/>
-      <i class="ri-search-line"></i>
-    </div>
-      </Col>
-    </Row>
+    <>
 
-    <Row>
-    {
-        searchedProduct.map((item)=>(
+      < div className='background-title'>
+
+        <h2>All Foods</h2>
+      </div>
+
+      <Container>
+        <Row>
           <Col lg="3">
-    <Foods item={item}/>
-    
-      </Col>
-        ))
-      }
-      
-    </Row>
-    
-  </Container>
-  
-  </>
+            <div className='search-box d-flex justify-content-between mt-5 mb-5'>
+              <input value={searchItem} onChange={e => setSearchItem((e).target.value)}
+                type='text' placeholder='Search' />
+              <i class="ri-search-line"></i>
+            </div>
+          </Col>
+        </Row>
+
+        <Row>
+          {
+            searchedProduct.map((item) => (
+              <Col lg="3">
+                <Foods item={item} />
+
+              </Col>
+            ))
+          }
+
+        </Row>
+
+      </Container>
+
+    </>
   )
 }
 
