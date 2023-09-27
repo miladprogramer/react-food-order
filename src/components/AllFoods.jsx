ac72de44bbd66475da76820e81fbb481421d436a
meysam <m.monfared83@gmail.com>
2023-09-27 22:22:42 +0330

    compleate search

--- src/components/AllFoods.jsx
+++ src/components/AllFoods.jsx
@@ -1,12 +1,24 @@
 
-import React from 'react'
+import {React,useState} from 'react'
 
 import { Container,Col,Row } from 'reactstrap'
 import './Style.css'
 
 import AllProducts from '../Fake-Data/FakeData'
-
+import Foods from './Foods'
 const AllFoods = () => {
+
+  const [searchItem,setSearchItem]=useState('')
+  
+  const searchedProduct = AllProducts.filter((item) => {
+    if (searchItem === "") {
+      return item;
+    }
+    if (item.title.toLowerCase().includes(searchItem.toLowerCase())) {
+      return item;
+    }
+  });
+
   return (
   <>
  
@@ -19,10 +31,23 @@ const AllFoods = () => {
     <Row>
       <Col lg="3">
       <div className='search-box d-flex justify-content-between mt-5 mb-5'>
-      <input type='text' placeholder='Search'/>
+      <input value={searchItem} onChange={e=> setSearchItem((e).target.value)}
+       type='text' placeholder='Search'/>
       <i class="ri-search-line"></i>
     </div>
       </Col>
+    </Row>
+
+    <Row>
+    {
+        searchedProduct.map((item)=>(
+          <Col lg="3">
+    <Foods item={item}/>
+    
+      </Col>
+        ))
+      }
+      
     </Row>
     
   </Container>
